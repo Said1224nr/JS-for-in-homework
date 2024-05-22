8d7e448da08a138b0cc8f446309ec6cb52c983ae
Said1224nr <162983493+Said1224nr@users.noreply.github.com>
2024-05-22 22:08:22 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,30 @@
+// let obj = {
+//     ID: 1962,
+//     password: 258963,
+//     name: "Saidakbar",
+//     isStudent: true
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+
+let obj = {
+    name: "Saidakbar",
+    age: 10,
+    City: "Toshkent"
+}
+
+console.log(obj);
+
+for (let value in obj) {
+    console.log(obj[value]);
+}
